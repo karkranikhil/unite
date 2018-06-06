d57c1e37871290486dca01b0a283a1e664ea2210
karkran <nikhil.karkra@anz.com>
2018-06-06 23:43:09 +1000

    setting up the controlled form and creating new events based on that

--- src/features/event/EventForm/EventForm.js
+++ src/features/event/EventForm/EventForm.js
@@ -2,30 +2,69 @@ import React, { Component } from 'react'
 import {Form, Segment, Button} from 'semantic-ui-react'
 
 class EventForm extends Component {
+state={
+    event:{
+        title:'',
+        date:'',
+        city:'',
+        venue:'',
+        hostedBy:''
+    }
+}
+    onFormSubmit=(event)=>{
+        event.preventDefault()
+        this.props.createEvent(this.state.event)
+    }
+    onInputChange=(evt)=>{
+        const newEvent = this.state.event
+        newEvent[evt.target.name] = evt.target.value
+        this.setState({
+            event:newEvent
+        })
+    }
   render() {
     const {handleCancel} = this.props
+    const{event}= this.state
     return (
             <Segment>
-              <Form>
+              <Form onSubmit={this.onFormSubmit}>
                 <Form.Field>
                   <label>Event Title</label>
-                  <input placeholder="First Name" />
+                  <input 
+                    value={event.title} 
+                    placeholder="Event Title"
+                    name='title'
+                    onChange={this.onInputChange}
+                    />
                 </Form.Field>
                 <Form.Field>
                   <label>Event Date</label>
-                  <input type="date" placeholder="Event Date" />
+                  <input 
+                    type="date" 
+                    placeholder="Event Date"
+                    name='date'
+                    onChange={this.onInputChange} />
                 </Form.Field>
                 <Form.Field>
                   <label>City</label>
-                  <input placeholder="City event is taking place" />
+                  <input 
+                    placeholder="City event is taking place" 
+                    name='city'
+                    onChange={this.onInputChange}/>
                 </Form.Field>
                 <Form.Field>
                   <label>Venue</label>
-                  <input placeholder="Enter the Venue of the event" />
+                  <input 
+                    placeholder="Enter the Venue of the event"
+                    name='venue'
+                    onChange={this.onInputChange} />
                 </Form.Field>
                 <Form.Field>
                   <label>Hosted By</label>
-                  <input placeholder="Enter the name of person hosting" />
+                  <input 
+                    placeholder="Enter the name of person hosting" 
+                    name='hostedBy'
+                    onChange={this.onInputChange}/>
                 </Form.Field>
                 <Button positive type="submit">
                   Submit
