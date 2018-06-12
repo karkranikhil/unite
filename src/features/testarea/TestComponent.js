9a3486abf15ce595f1d32abef38e806f1bf1bc70
karkran <nikhil.karkra@anz.com>
2018-06-12 23:49:47 +1000

    cleaner way of writing the reducer

--- src/features/testarea/TestComponent.js
+++ src/features/testarea/TestComponent.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { incrementCounter, decrementCounter } from './testActions';
 
+import { Button } from 'semantic-ui-react';
 
 class TestComponent extends Component {
   render() {
+      const {incrementCounter, decrementCounter,data } = this.props
     return (
       <div>
        <h1>Test Component</h1> 
-       <h3>Than answer is : {this.props.data}</h3>
+       <h3>Than answer is : {data}</h3>
+       <Button onClick={incrementCounter} color="green" content ='Increment'/>
+       <Button onClick={decrementCounter} color="red" content ='Decrement'/>
+
       </div>
     )
   }
@@ -16,4 +22,9 @@ class TestComponent extends Component {
 const mapState = (state)=>({
     data:state.test.data
 })
-export default connect(mapState)(TestComponent);
+
+const actions = {
+    incrementCounter,
+    decrementCounter
+}
+export default connect(mapState, actions)(TestComponent);
